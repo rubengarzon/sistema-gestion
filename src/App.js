52a052c34eee11433267da9b76519ccadefd1f9c
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-08-01 11:58:45 +0200

    navbar añadido

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Navbar/>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import "../App.css";
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li className="navbar-li">
+          <a href="">Clientes</a>
+          <a href="">Productos</a>
+          <a href="">Iniciar Sesión</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
